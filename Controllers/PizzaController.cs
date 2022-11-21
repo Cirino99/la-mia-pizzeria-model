c8215e873445169ef296a84e89778e42a3cac364
Simone <cirino.simone99@gmail.com>
2022-11-21 14:09:12 +0100

    creato db, PizzaController e home dinamica

---
+++ Controllers/PizzaController.cs
@@ -0,0 +1,16 @@
+﻿using la_mia_pizzeria_static.Data;
+using la_mia_pizzeria_static.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace la_mia_pizzeria_static.Controllers
+{
+    public class PizzaController : Controller
+    {
+        public IActionResult Index()
+        {
+            PizzeriaDbContext db = new PizzeriaDbContext();
+            List<Pizza> pizze = db.Pizze.ToList();
+            return View(pizze);
+        }
+    }
+}
